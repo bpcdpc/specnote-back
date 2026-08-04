cffc8a51b555b63c2b451c2c3894747562b93e31
bpcdpc <bpcdpc@hanmail.net>
2026-08-04 15:29:08 +0900

    chore: CORS 허용 오리진을 환경변수로 분리 (#20)

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  InternalServerErrorException,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { SummaryInput } from '../comments/comments.type';
 
 @Injectable()
